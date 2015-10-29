cd55502cadb5677230270817b3dc518c294152fc
Suprada Urval <suprada@Supradas-MacBook-Pro.local>
2015-10-29 10:57:33 -0700

    The Sea Theme for Flogr

---
+++ themes/sea/header.php
@@ -0,0 +1,15 @@
+<!-- Javascript effects -->
+<link href='<?php echo SITE_URL;?>/<?php echo SITE_THEME_PATH;?>css/slimbox2.css' rel='stylesheet' type='text/css' />
+<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/themes/black-tie/jquery-ui.css" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js"></script>
+<script type='text/javascript' src='<?php echo SITE_URL; ?>/<?php echo SITE_THEME_PATH;?>js/slimbox2.js'></script>
+  <div id="container">
+
+    <div id="wrapper">
+    
+      <div id="header">
+        <span id="header_title">Sea Theme for <a href="http://code.google.com/p/flogr/" target='_blank'>Flogr</a></span> 
+		<span id="header_title2"> | Designed by <a href="http://www.suprada.com" target='_blank'>Suprada</a></span> 
+        <span id="header_menu"><?php include('menu.php'); ?></span>
+      </div>
