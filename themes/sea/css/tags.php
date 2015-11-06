471fcf9d2afae6c6ea59ad9e956bbfafd183b962
Suprada Urval <suprada@Supradas-MacBook-Pro.local>
2015-11-05 20:40:50 -0800

    commit for gh-gages

---
+++ themes/sea/css/tags.php
@@ -0,0 +1,31 @@
+<?php $p = new Profiler('Tags page load', 0, 2); ?>
+<?php
+require_once( 'user.php' );
+?>
+      <div id="leftcontent"></div>
+
+      <div id="centercontent">
+        <div id='page_header'>
+          <span id='page_title'>
+          	Tags
+          </span>
+        </div>
+
+        <div id='page'>
+
+            <!-- Show the photo and link the photo to the previous photo -->
+            <div id='thumbnail_container'>
+            <div id='tag_container'>
+          	<?php $user->tag_list_popular(null, '<span>', '</span'); ?>
+            </div>
+            </div>
+        </div>
+      </div>
+
+      <div id="rightcontent"></div>
+      
+      <script type='text/javascript'>
+          $(document).ready(function(){
+              $("#menuitem_tags").addClass("selected");
+          });
+      </script>
